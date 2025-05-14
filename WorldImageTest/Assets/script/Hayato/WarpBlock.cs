e82f1b66556c67450070c0ce87cdade670b4f1de
Hayato222 <g2227070@st.t-kougei.ac.jp>
2025-05-15 00:08:13 +0900

    テレポート時のフェイドインアウトを実装中

--- WorldImageTest/Assets/script/Hayato/WarpBlock.cs
+++ WorldImageTest/Assets/script/Hayato/WarpBlock.cs
@@ -1,3 +1,4 @@
+using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -5,17 +6,38 @@ using VRC.Udon;
 
 public class WarpBlock : UdonSharpBehaviour
 {
-    [SerializeField] private Transform WarpPosition;
-    private Vector3 PlayerPosition;
+    [UdonSynced]
+    private bool isOn = false;
+    
+    [Header("ワープ先")]
+    [SerializeField] private GameObject WarpPoint;
+    private Quaternion PlayerRotate;
+    private Vector3 WarpPosition;
+    [Header("暗転UI")]
+    public GameObject fadeCanvas;         // Canvas本体
+    private Animator fadeAnimator;
+
+    private void Start()
+    {
+        WarpPosition = WarpPoint.transform.position;
+        fadeCanvas.SetActive(false);
+        fadeAnimator = fadeCanvas.GetComponent<Animator>();
+    }
 
     public override void Interact()
     {
-        PlayerPosition = Networking.LocalPlayer.GetPosition();
-        
+        fadeCanvas.SetActive(true);
+        PlayerRotate = new Quaternion(0, -180, 0, 0);
+        Networking.LocalPlayer.TeleportTo(WarpPosition,PlayerRotate);
     }
 
-    private void Warp()
+    public void DoFade()
     {
-        
+        fadeAnimator.SetTrigger("FadeIn");
+    }
+
+    public void EndFade()
+    {
+        fadeCanvas.SetActive(false);
     }
 }
