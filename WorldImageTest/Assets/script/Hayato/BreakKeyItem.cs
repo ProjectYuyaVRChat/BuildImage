579bac8c18d6b8ac12d361a4fd3ef788c2c156f9
Hayato222 <g2227070@st.t-kougei.ac.jp>
2025-05-26 17:42:31 +0900

    それっぽくなった

---
+++ WorldImageTest/Assets/script/Hayato/BreakKeyItem.cs
@@ -0,0 +1,46 @@
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BreakKeyItem : UdonSharpBehaviour
+{
+    public GameObject Door;
+    public GameObject HummerHead;
+    [Tooltip("同期先の球体")] 
+    public GameObject[] m_SyncTargets;
+    
+    private void Start()
+    {
+        if (m_SyncTargets != null)
+        {
+            foreach (var target in m_SyncTargets)
+            {
+                if (target == null) continue;
+                if (target.GetComponent<SyncObject>() != null)
+                {
+                    Debug.LogWarning("SyncObjectの同期先「{target.name}」にも同スクリプトが付いてるけど大丈夫そ？");
+                    break;
+                }
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == HummerHead)
+        {
+            if (m_SyncTargets != null)
+            {
+                foreach (var target in m_SyncTargets)
+                {
+                    if (target == null) continue;
+                    Door.SetActive(false);
+                    Destroy(gameObject);
+                    Destroy(target);
+                }
+            }
+        }
+    }
+}
