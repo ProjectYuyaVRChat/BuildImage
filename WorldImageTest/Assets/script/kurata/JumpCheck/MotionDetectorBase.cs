6ef6d2e50426648bfe021316468d05fa5a04e357
kqlnei <kouami0907@icloud.com>
2025-06-04 12:10:17 +0900

    ActionClass
    アクションの基底クラス
    Jumpの子クラス
    wordSpaceTextCanvas設定

---
+++ WorldImageTest/Assets/script/kurata/JumpCheck/MotionDetectorBase.cs
@@ -0,0 +1,24 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public abstract class MotionDetectorBase : UdonSharpBehaviour
+{
+    protected VRCPlayerApi localPlayer;
+
+    void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+    }
+
+    void Update()
+    {
+        if (localPlayer == null) return;
+
+        DetectMotion();
+    }
+
+    // 継承先で動作検出を実装する抽象メソッド
+    protected abstract void DetectMotion();
+}
