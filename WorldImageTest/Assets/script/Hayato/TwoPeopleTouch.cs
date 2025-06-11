3259a64a2ef84439e1c44038c2f06a83a754d595
Hayato <g2227070@st.t-kougei.ac.jp>
2025-06-11 15:24:50 +0900

    同期成功

--- WorldImageTest/Assets/script/Hayato/TwoPeopleTouch.cs
+++ WorldImageTest/Assets/script/Hayato/TwoPeopleTouch.cs
@@ -3,11 +3,14 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
 
 public class TwoPeopleTouch : UdonSharpBehaviour
 {
-    public WarpBlock PastWarpBlock;
-    public WarpBlock FutureWarpBlock;
+    [SerializeField] private WarpBlock PastWarpBlock;
+    [SerializeField] private WarpBlock FutureWarpBlock;
+    
+    private bool isSequenceActive = false;
 
     void Start()
     {
@@ -24,12 +27,47 @@ public class TwoPeopleTouch : UdonSharpBehaviour
     
     void Update()
     {
+        if (isSequenceActive)
+        {
+            return;
+        }
+        
         if (PastWarpBlock.isOn && FutureWarpBlock.isOn)
         {
-            PastWarpBlock.DoFade();
-            FutureWarpBlock.DoFade();
-            PastWarpBlock.isOn = false;
-            FutureWarpBlock.isOn = false;
+            isSequenceActive = true; // 即座に再判定されないようにロックする
+            
+            // 全プレイヤーに対して「ワープを開始せよ」という命令を送る
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(TriggerAllClients));
+
+            // --- 状態のリセット ---
+            // 3秒後にマスターが各ブロックの状態をリセットする
+            SendCustomEventDelayedSeconds(nameof(MasterResetBlocks), 3.0f);
         }
+    }
+    
+    public void TriggerAllClients()
+    {
+        // 各ブロックにワープ開始を命令
+        // ブロック内部のロジックにより、実際にワープするのは操作した本人のみ
+        if (PastWarpBlock != null) PastWarpBlock.Warp();
+        if (FutureWarpBlock != null) FutureWarpBlock.Warp();
+    }
+    
+    // マスターのみで実行されるリセット処理
+    public void MasterResetBlocks()
+    {
+        // マスターが各ブロックのオーナーになり、リセットを命令する
+        if (PastWarpBlock != null)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, PastWarpBlock.gameObject);
+            PastWarpBlock.ResetState();
+        }
+        if (FutureWarpBlock != null)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, FutureWarpBlock.gameObject);
+            FutureWarpBlock.ResetState();
+        }
+        
+        isSequenceActive = false; // 再度、判定できるようにロックを解除する
     }
 }
--- WorldImageTest/Assets/script/Hayato/WarpBlock.cs
+++ WorldImageTest/Assets/script/Hayato/WarpBlock.cs
@@ -8,6 +8,10 @@ public class WarpBlock : UdonSharpBehaviour
 {
     [UdonSynced]
     public bool isOn = false;
+
+    [UdonSynced]
+    private int interactingPlayerId = -1;
+    
     
     [Header("ワープ先")]
     [SerializeField] private GameObject warpPoint;
@@ -17,7 +21,7 @@ public class WarpBlock : UdonSharpBehaviour
     public GameObject fadeCanvas;         // Canvas本体
     private Animator fadeAnimator;
     
-    private VRCPlayerApi player;
+    private bool isSequenceRunning = false;
 
     private void Start()
     {
@@ -28,27 +32,72 @@ public class WarpBlock : UdonSharpBehaviour
 
     public override void Interact()
     {
-        isOn = true;
-        player = Networking.LocalPlayer;
+        if (isOn) return;
+        
+        // このオブジェクトのオーナーを自分自身に設定し、同期変数を変更する権限を得る
+        Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+        
+        // 状態を更新
+        this.isOn = true;
+        this.interactingPlayerId = Networking.LocalPlayer.playerId;
+        
+        // 同期変数の変更を他のプレイヤーに通知
+        RequestSerialization();
     }
 
     public void Warp()
     {
-        Debug.Log("aaaaa");
-        PlayerRotate = new Quaternion(0, -180, 0, 0);
-        player.TeleportTo(WarpPosition,PlayerRotate);
+        // 連続で実行されないようにガード
+        if (isSequenceRunning) return;
+        
+        // 誰も操作していない場合は何もしない
+        if (interactingPlayerId == -1) return;
+        
+        // このブロックを操作した本人だけがワープ処理を実行する
+        VRCPlayerApi playerToWarp = VRCPlayerApi.GetPlayerById(interactingPlayerId);
+        if (playerToWarp != null && playerToWarp.isLocal)
+        {
+            isSequenceRunning = true;
+            DoFade();
+        }
     }
     
     public void DoFade()
     {
         fadeCanvas.SetActive(true);
         fadeAnimator.SetTrigger("FadeIn");
-        SendCustomEventDelayedSeconds(nameof(Warp),1);
+        SendCustomEventDelayedSeconds(nameof(WarpPlayer),1);
         SendCustomEventDelayedSeconds(nameof(EndFade),2);
+    }
+    
+    public void WarpPlayer()
+    {
+        VRCPlayerApi playerToWarp = VRCPlayerApi.GetPlayerById(interactingPlayerId);
+        if (playerToWarp != null && playerToWarp.isLocal)
+        {
+            playerToWarp.TeleportTo(
+                warpPoint.transform.position,
+                Quaternion.Euler(0, 180f, 0)
+            );
+        }
     }
 
     public void EndFade()
     {
-        fadeCanvas.SetActive(false);
+        if (fadeCanvas != null)
+        {
+            fadeCanvas.SetActive(false);
+        }
+        isSequenceRunning = false;
+    }
+    
+    public void ResetState()
+    {
+        this.isOn = false;
+        this.interactingPlayerId = -1;
+        this.isSequenceRunning = false;
+        
+        // リセットした状態を全員に同期
+        RequestSerialization();
     }
 }
