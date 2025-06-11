db034bbe3e55d8de619050d6b34ddd9500d613a6
kqlnei <kouami0907@icloud.com>
2025-06-11 17:03:22 +0900

    首の傾きと回転検知

---
+++ WorldImageTest/Assets/script/kurata/MotionCheck/AnyCheck/Head/HeadTiltAllDirectionDetector.cs
@@ -0,0 +1,73 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public enum NeckTiltState
+{
+    Default,
+    TiltLeft,
+    TiltRight,
+    TiltForward,
+    TiltBackward
+}
+public class HeadTiltAllDirectionDetector : MotionDetectorBase
+{
+    private NeckTiltState currentState = NeckTiltState.Default;
+
+    // 傾き判定の閾値（度数）
+    [SerializeField] private float tiltThreshold = 15f;
+
+    protected override void DetectMotion()
+    {
+        Quaternion headRot = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+        Vector3 euler = headRot.eulerAngles;
+
+        // Euler角は0〜360なので -180〜180 に変換
+        float roll = euler.z > 180f ? euler.z - 360f : euler.z;   // 左右傾き
+        float pitch = euler.x > 180f ? euler.x - 360f : euler.x;  // 前後傾き
+
+        NeckTiltState newState = NeckTiltState.Default;
+
+        if (roll > tiltThreshold)
+        {
+            newState = NeckTiltState.TiltRight;
+        }
+        else if (roll < -tiltThreshold)
+        {
+            newState = NeckTiltState.TiltLeft;
+        }
+        else if (pitch > tiltThreshold)
+        {
+            newState = NeckTiltState.TiltForward;
+        }
+        else if (pitch < -tiltThreshold)
+        {
+            newState = NeckTiltState.TiltBackward;
+        }
+
+        if (newState != currentState)
+        {
+            currentState = newState;
+            switch (currentState)
+            {
+                case NeckTiltState.TiltLeft:
+                    ShowMotionMessage("首を左に傾けた");
+                    break;
+                case NeckTiltState.TiltRight:
+                    ShowMotionMessage("首を右に傾けた");
+                    break;
+                case NeckTiltState.TiltForward:
+                    ShowMotionMessage("首を前に傾けた");
+                    break;
+                case NeckTiltState.TiltBackward:
+                    ShowMotionMessage("首を後ろに傾けた");
+                    break;
+                case NeckTiltState.Default:
+                    ShowMotionMessage("首を戻した");
+                    break;
+            }
+        }
+    }
+}
---
+++ WorldImageTest/Assets/script/kurata/MotionCheck/AnyCheck/Head/HeadTurnDetector.cs
@@ -0,0 +1,65 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+
+public enum HeadTurnState
+{
+    Default,
+    LookingLeft,
+    LookingRight
+}
+
+public class HeadTurnDetector : MotionDetectorBase
+{
+    private HeadTurnState currentState = HeadTurnState.Default;
+
+    [SerializeField] private float turnThreshold = 20f; // Yaw差
+
+    protected override void DetectMotion()
+    {
+        Quaternion bodyRot = localPlayer.GetRotation();
+        Quaternion headRot = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+
+        float bodyYaw = bodyRot.eulerAngles.y;
+        float headYaw = headRot.eulerAngles.y;
+
+        // 差分を-180〜180の範囲にする
+        float yawDiff = Mathf.DeltaAngle(bodyYaw, headYaw);
+
+        HeadTurnState newState = HeadTurnState.Default;
+
+        if (yawDiff > turnThreshold)
+        {
+            newState = HeadTurnState.LookingRight;
+        }
+        else if (yawDiff < -turnThreshold)
+        {
+            newState = HeadTurnState.LookingLeft;
+        }
+
+        if (newState != currentState)
+        {
+            currentState = newState;
+            switch (currentState)
+            {
+                case HeadTurnState.LookingLeft:
+                    ShowMotionMessage("首を左に向けた");
+                    break;
+                case HeadTurnState.LookingRight:
+                    ShowMotionMessage("首を右に向けた");
+                    break;
+                case HeadTurnState.Default:
+                    ShowMotionMessage("首の向きを戻した");
+                    break;
+            }
+        }
+
+        // 戻したときもNeutralに戻す（±turnThresholdの範囲内）
+        if (currentState != HeadTurnState.Default && Mathf.Abs(yawDiff) <= turnThreshold)
+        {
+            currentState = HeadTurnState.Default;
+            ShowMotionMessage("首の向きを戻した");
+        }
+    }
+}
