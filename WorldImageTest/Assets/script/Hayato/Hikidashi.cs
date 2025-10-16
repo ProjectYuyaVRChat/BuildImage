190fb3d726204b7e49df1889dd42743c4134d9bc
Hayato222 <g2227070@st.t-kougei.ac.jp>
2025-10-16 18:01:57 +0900

    完了！

--- WorldImageTest/Assets/script/Hayato/Hikidashi.cs
+++ WorldImageTest/Assets/script/Hayato/Hikidashi.cs
@@ -18,6 +18,8 @@ public class Hikidashi : UdonSharpBehaviour
     public bool x = false;
     public bool y = false;
     public bool z = false;
+    public float maxDistance;
+    public bool minus = false;
     
     void Start()
     {
@@ -33,18 +35,90 @@ public class Hikidashi : UdonSharpBehaviour
         {
             currentX = transform.position.x;
             newPosition = new Vector3(currentX, hikiObj.y, hikiObj.z);
+            if (minus)
+            {
+                if (maxDistance <= hikiObj.x - currentX)
+                {
+                    newPosition = new Vector3(hikiObj.x - maxDistance, hikiObj.y, hikiObj.z);
+                }
+
+                if (0 <= currentX - hikiObj.x)
+                {
+                    newPosition = hikiObj;
+                }
+            }
+            else
+            {
+                if (maxDistance <=currentX - hikiObj.x)
+                {
+                    newPosition = new Vector3(hikiObj.x + maxDistance, hikiObj.y, hikiObj.z);
+                }
+
+                if (0 <= hikiObj.x - currentX)
+                {
+                    newPosition = hikiObj;
+                }
+            }
         }
         
         if (y)
         {
             currentY = transform.position.y;
             newPosition = new Vector3(hikiObj.x, currentY, hikiObj.z);
+            if (minus)
+            {
+                if (maxDistance <= hikiObj.y - currentY)
+                {
+                    newPosition = new Vector3(hikiObj.x, hikiObj.y - maxDistance, hikiObj.z);
+                }
+
+                if (0 <= currentY - hikiObj.y)
+                {
+                    newPosition = hikiObj;
+                }
+            }
+            else
+            {
+                if (maxDistance <=currentY - hikiObj.y)
+                {
+                    newPosition = new Vector3(hikiObj.x, hikiObj.y + maxDistance, hikiObj.z);
+                }
+
+                if (0 <= hikiObj.y - currentY)
+                {
+                    newPosition = hikiObj;
+                }
+            }
         }
 
         if (z)
         {
             currentZ = transform.position.z;
             newPosition = new Vector3(hikiObj.x, hikiObj.y, currentZ);
+            if (minus)
+            {
+                if (maxDistance <= hikiObj.z - currentZ)
+                {
+                    newPosition = new Vector3(hikiObj.x, hikiObj.y, hikiObj.z - maxDistance);
+                }
+
+                if (0 <= currentZ - hikiObj.z)
+                {
+                    newPosition = hikiObj;
+                }
+            }
+            else
+            {
+                if (maxDistance <=currentZ - hikiObj.z)
+                {
+                    newPosition = new Vector3(hikiObj.x, hikiObj.y, hikiObj.z + maxDistance);
+                }
+
+                if (0 <= hikiObj.z - currentZ)
+                {
+                    newPosition = hikiObj;
+                }
+            }
         }
 
         transform.position = newPosition;
