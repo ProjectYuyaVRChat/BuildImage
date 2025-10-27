5054911965ee4edf2e07ccd3b48d46b06566b60a
Hayato <g2227070@st.t-kougei.ac.jp>
2025-10-27 16:55:49 +0900

    いったん完成　同期化済み

--- WorldImageTest/Assets/script/Hayato/Camera/PhotoTaker.cs
+++ WorldImageTest/Assets/script/Hayato/Camera/PhotoTaker.cs
@@ -7,11 +7,22 @@ using VRC.Udon;
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class PhotoTaker : UdonSharpBehaviour
 {
-    [SerializeField] private Camera worldCamera;
+    [SerializeField]private Camera worldCamera;
 
     [UdonSynced]private bool isCapturing = true;
-    [SerializeField]private TextMeshProUGUI debugText;
+    /*[SerializeField]private TextMeshProUGUI debugText;
+    [SerializeField]private TextMeshProUGUI debugText2;
+    [SerializeField]private TextMeshProUGUI debugText3;
+    private float counter = 0;*/
 
+    private void Start()
+    {
+        updateCameraState();
+        if (Networking.IsOwner(gameObject))
+        {
+            RequestSerialization();
+        }
+    }
     public void ToggleCapturing()
     {
         if (!Networking.IsOwner(gameObject))
@@ -19,31 +30,40 @@ public class PhotoTaker : UdonSharpBehaviour
             Networking.SetOwner(Networking.LocalPlayer, gameObject);
         }
         
-        isCapturing = !isCapturing;
-        
-        RequestSerialization();
         updateCameraState();
+        isCapturing = !isCapturing;
+        RequestSerialization();
     }
 
     public override void OnDeserialization()
     {
+        /*counter++;
+        if (debugText2 != null)
+        {
+            debugText2.text = ($"PhotoTaker: OnDeserializationを受信しました。({counter}) isCapturingは {isCapturing} です。");
+        }*/
         updateCameraState();
     }
 
     private void updateCameraState()
     {
+        /*if (worldCamera == null)
+        {
+            debugText3.text = ("PhotoTaker: worldCameraがインスペクタで設定されていません。");
+            return;
+        }*/
+        
         worldCamera.gameObject.SetActive(isCapturing);
-        RequestSerialization();
 
         if (isCapturing)
         {
             Debug.Log("PhotoTaker: ライブキャプチャを再開");
-            debugText.text = "PhotoTaker: ライブキャプチャを再開";
+            /*debugText.text = "PhotoTaker: ライブキャプチャを再開";*/
         }
         else
         {
-            Debug.Log("PhatoTaker: 静止画として固定");
-            debugText.text = "PhatoTaker: 静止画として固定";
+            Debug.Log("PhotoTaker: 静止画として固定");
+            /*debugText.text = "PhotoTaker: 静止画として固定";*/
         }
     }
 }
