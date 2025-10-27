ed986fd8ee386a7802774208874970a2b7f22a1f
Hayato <g2227070@st.t-kougei.ac.jp>
2025-10-27 18:08:40 +0900

    調整中

--- WorldImageTest/Assets/script/Hayato/Bridge/Respawn.cs
+++ WorldImageTest/Assets/script/Hayato/Bridge/Respawn.cs
@@ -1,6 +1,7 @@
 using System;
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UIElements;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -10,15 +11,35 @@ public class Respawn : UdonSharpBehaviour
     [SerializeField] private Transform respawnPoint2;
     public PassMidPoint mid;
 
+    [SerializeField] private float respawnRotateY;
+    private Quaternion startRespawnRotation;
+    private Quaternion midRespawnRotation;
+
+    private void Start()
+    {
+        startRespawnRotation = Quaternion.Euler(
+            respawnPoint.rotation.eulerAngles.x,
+            respawnRotateY, 
+            respawnPoint.rotation.eulerAngles.z
+        );
+        
+        midRespawnRotation = Quaternion.Euler(
+            respawnPoint2.rotation.eulerAngles.x,
+            respawnRotateY, 
+            respawnPoint2.rotation.eulerAngles.z
+        );
+    }
+
     public override void OnPlayerTriggerEnter(VRCPlayerApi player)
     {
+        Debug.Log("passssssssssssss");
         if (!mid.isOn)
         {
-            player.TeleportTo(respawnPoint.position, respawnPoint.rotation);
+            player.TeleportTo(respawnPoint.position, startRespawnRotation);
         }
         else
         {
-            player.TeleportTo(respawnPoint2.position, respawnPoint2.rotation);
+            player.TeleportTo(respawnPoint2.position, midRespawnRotation);
         }
     }
 }
