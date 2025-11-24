b5383fadb8e0d626e9db049e8eda9654715a7fe6
kqlnei <kouami0907@icloud.com>
2025-11-24 14:01:42 +0900

    画像出るようになった

---
+++ WorldImageTest/Assets/script/kurata/1motion/MotionObjectActivator.cs
@@ -0,0 +1,68 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MotionObjectActivator : UdonSharpBehaviour
+{
+    [Header("モーションに対応するオブジェクト")]
+    [SerializeField] private GameObject motion1Object;
+    [SerializeField] private GameObject motion2Object;
+    [SerializeField] private GameObject motion3Object;
+
+    [Header("対応モーションタイプ")]
+    [SerializeField] private MotionType motion1 = MotionType.Jump;
+    [SerializeField] private MotionType motion2 = MotionType.RightHandUp;
+    [SerializeField] private MotionType motion3 = MotionType.HeadTiltLeft;
+
+    // 外部からモーション成功を通知するメソッド
+    public void SetMotionSuccess(MotionType motionType)
+    {
+        if (motionType == motion1)
+        {
+            if (motion1Object != null)
+            {
+                motion1Object.SetActive(true);
+            }
+        }
+        else if (motionType == motion2)
+        {
+            if (motion2Object != null)
+            {
+                motion2Object.SetActive(true);
+            }
+        }
+        else if (motionType == motion3)
+        {
+            if (motion3Object != null)
+            {
+                motion3Object.SetActive(true);
+            }
+        }
+    }
+
+    // 便利メソッド：MotionType ではなく直接モーション番号で設定
+    public void SetMotion1Success()
+    {
+        if (motion1Object != null)
+        {
+            motion1Object.SetActive(true);
+        }
+    }
+
+    public void SetMotion2Success()
+    {
+        if (motion2Object != null)
+        {
+            motion2Object.SetActive(true);
+        }
+    }
+
+    public void SetMotion3Success()
+    {
+        if (motion3Object != null)
+        {
+            motion3Object.SetActive(true);
+        }
+    }
+}
---
+++ WorldImageTest/Assets/script/kurata/1motion/MotionSuccess.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class MotionSuccess : UdonSharpBehaviour
+{
+    [Header("ドアギミック本体")]
+    public DoorGimmickSystemNew gimmick;
+
+    [Header("成功時に ON になるオブジェクト")]
+    public GameObject motion1Object;
+    public GameObject motion2Object;
+    public GameObject motion3Object;
+
+    void Update()
+    {
+        if (gimmick == null) return;
+
+        // 1つ目のモーション
+        if (motion1Object != null)
+            motion1Object.SetActive(gimmick.IsMotion1Success());
+
+        // 2つ目のモーション
+        if (motion2Object != null)
+            motion2Object.SetActive(gimmick.IsMotion2Success());
+
+        // 3つ目のモーション
+        if (motion3Object != null)
+            motion3Object.SetActive(gimmick.IsMotion3Success());
+    }
+}
