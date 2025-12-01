3b2bceeb525cbf5ed8935592b040d2a31b80ef1d
kqlnei <kouami0907@icloud.com>
2025-12-01 09:49:06 +0900

    最後だけドア開く
    mosionroom (3)にMotionToggleってスクリプトつけたからそれ見ながら配置してください

---
+++ WorldImageTest/Assets/script/kurata/MotionCheck/MotionToggle.cs
@@ -0,0 +1,48 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MotionToggle : UdonSharpBehaviour
+{
+   [Header("ギミック本体")]
+    public DoorGimmickSystemNew gimmickA;
+    public DoorGimmickSystemNew gimmickB;
+    public DoorGimmickSystemNew gimmickC;
+
+    [Header("制御するオブジェクト")]
+    public GameObject objectA;
+    public GameObject objectB;
+    public GameObject objectC;
+
+    // すでに処理済みかどうかのフラグ
+    private bool triggeredA = false;
+    private bool triggeredB = false;
+    private bool triggeredC = false;
+
+    void Update()
+    {
+        // --- A ---
+        if (!triggeredA && gimmickA != null && gimmickA.IsMotion1Success())
+        {
+            triggeredA = true;
+            if (objectA != null) objectA.SetActive(false);
+            if (objectB != null) objectB.SetActive(true);
+        }
+
+        // --- B ---
+        if (!triggeredB && gimmickB != null && gimmickB.IsMotion1Success())
+        {
+            triggeredB = true;
+            if (objectB != null) objectB.SetActive(false);
+            if (objectC != null) objectC.SetActive(true);
+        }
+
+        // --- C ---
+        if (!triggeredC && gimmickC != null && gimmickC.IsMotion1Success())
+        {
+            triggeredC = true;
+            if (objectC != null) objectC.SetActive(false);
+        }
+    }
+}
