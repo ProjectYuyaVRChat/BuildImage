92521df61d254b04c9baaa719c4ae360e66a4c44
kqlnei <kouami0907@icloud.com>
2026-01-19 01:11:21 +0900

    siritori

---
+++ WorldImageTest/Assets/script/kurata/siritoti/BoxSlot.cs
@@ -0,0 +1,28 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class BoxSlot : UdonSharpBehaviour
+{
+    [Header("この箱に入れる正解オブジェクト")]
+    public GameObject correctObject;
+
+    [HideInInspector]
+    public bool isCorrect = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == correctObject)
+        {
+            isCorrect = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == correctObject)
+        {
+            isCorrect = false;
+        }
+    }
+}
