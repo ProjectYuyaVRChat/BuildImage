f710611fabba961c53059509d897f6088dca3a72
kqlnei <kouami0907@icloud.com>
2026-01-20 05:48:51 +0900

    aaaaa

---
+++ WorldImageTest/Assets/script/kurata/MotionCheck/TouchSoundTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TouchSoundTrigger : UdonSharpBehaviour
+{
+    [Header("反応するオブジェクト")]
+    public GameObject targetObject;
+
+    [Header("再生する音")]
+    public AudioClip sound;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == targetObject)
+        {
+            if (sound)
+            {
+                audioSource.PlayOneShot(sound);
+            }
+        }
+    }
+}
